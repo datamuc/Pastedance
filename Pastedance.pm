557b558ebe77f60edb9c70e9a8f02eb7f6b91a69
Danijel Tasov <dt@korn.shell.la>
2010-03-10 23:56:12 +0100

    use perl to highlight perl

--- Pastedance.pm
+++ Pastedance.pm
@@ -7,6 +7,19 @@ use Data::Uniqid qw/uniqid/;
 use lib '/opt/sh';
 use Encode qw/decode encode/;
 use SourceHighlight;
+use Syntax::Highlight::Perl::Improved;
+use KiokuDB::Backend::MongoDB;
+use MongoDB;
+
+# my $conn = MongoDB::Connection->new(host => 'localhost');
+# my $mongodb    = $conn->get_database('Pastedance');
+# my $collection = $mongodb->get_collection('Pastedance');
+# my $mongo = KiokuDB::Backend::MongoDB->new('collection' => $collection);
+# 
+# my $k = KiokuDB->new(
+#   backend => $mongo
+# );
+# 
 
 my $k = KiokuDB->connect(
   #"dbi:SQLite:dbname=pastedance.db",
@@ -89,6 +102,10 @@ get '/plain/:id' => sub {
 sub highlight {
   my $doc = shift;
   my $ln  = shift;
+  warn "xxx " .$doc->{code};
+  if($doc->{lang} eq "Perl") {
+    return perl_highlight($doc, $ln);
+  }
   my $lang = config->{langs}->{$doc->{lang}} // 'nohilite.lang';
   $doc->{code} =~ s/\t/        /g;
   my $hl = SourceHighlight::SourceHighlight->new('html.outlang');
@@ -101,6 +118,52 @@ sub highlight {
   return $hl->highlightString($doc->{code}, $lang);
 }
 
+sub perl_highlight {
+  my $doc = shift;
+  my $ln  = shift;
+  
+  warn "xxx " .$doc->{code};
+  my %default_styles = (
+          'Comment_Normal'    => 'color:#006699;font-style:italic;',
+          'Comment_POD'       => 'color:#001144;font-style:italic;',
+          'Directive'         => 'color:#339999;font-style:italic;',
+          'Label'             => 'color:#993399;font-style:italic;',
+          'Quote'             => 'color:#0000aa;',
+          'String'            => 'color:#0000aa;',
+          'Subroutine'        => 'color:#998800;',
+          'Variable_Scalar'   => 'color:#008800;',
+          'Variable_Array'    => 'color:#ff7700;',
+          'Variable_Hash'     => 'color:#8800ff;',
+          'Variable_Typeglob' => 'color:#ff0033;',
+          'Whitespace'        => '',
+          'Character'         => 'color:#880000;',
+          'Keyword'           => 'color:#000000;',
+          'Builtin_Operator'  => 'color:#330000;',
+          'Builtin_Function'  => 'color:#000011;',
+          'Operator'          => 'color:#000000;',
+          'Bareword'          => 'color:#33AA33;',
+          'Package'           => 'color:#990000;',
+          'Number'            => 'color:#ff00ff;',
+          'Symbol'            => 'color:#000000;',
+          'CodeTerm'          => 'color:#000000;',
+          'DATA'              => 'color:#000000;',
+          'LineNumber'        => 'color:#CCCCCC;'
+  );
+
+  my $formatter = new Syntax::Highlight::Perl::Improved;
+  $formatter->define_substitution(
+    '<' => '&lt;', '>' => '&gt;', '&' => '&amp;'
+  ); 
+
+  while ( my($type,$style) = each %default_styles ) {
+    $formatter->set_format($type, [ "<span style=\"$style\">",'</span>' ] );
+    #$str = '<PRE style="font-size:10pt;color:#333366;">';
+  }
+
+  my @lines = $formatter->format_string($doc->{code});
+  return '<pre>'.join("",@lines).'</pre>';
+}
+
 sub e404 {
   status '404';
   content_type 'text/plain';
@@ -108,3 +171,5 @@ sub e404 {
 }
 
 true;
+
+# vim: sw=2 ai et
