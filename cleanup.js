57f0761e2985b2f2140f8a17d020812eb114400a
Danijel Tasov <dt@korn.shell.la>
2010-10-30 23:27:58 +0200

    fixed cleanup.js

--- cleanup.js
+++ cleanup.js
@@ -1,10 +1,9 @@
-db.Pastedance.remove({
-    $where: function() {
-        now = (new Date).getTime() / 1000;
-        if(this.time == -1) {
-            return false
+db.Pastedance.remove({$where: function() {
+        var now = parseInt((new Date).getTime() / 1000);
+        if (this.time == -1) {
+            return false;
         } else {
-            return this.time + this.expires < now
+            return this.time + parseInt(this.expires)  <  now;
         }
     }
 });
