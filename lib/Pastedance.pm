7f0a1e5a5fb6774409d2f83a596f1e9387c87b0c
Danijel Tasov <dt@korn.shell.la>
2010-11-05 17:55:34 +0100

    cleanup

--- lib/Pastedance.pm
+++ lib/Pastedance.pm
@@ -44,7 +44,7 @@ post '/' => sub {
     my $lang = request->params->{lang};
     my $subject = request->params->{subject};
     unless(length($code)) {
-      return halt("Don't paste no code");
+        return send_error("Don't paste no code!", 403);
     }
 
     my %rlex = reverse %{ get_lexers() };
