a73925e1ed2181bc53b1b1141311cd67ada8ef93
Danijel Tasov <m@rbfh.de>
2017-05-23 23:46:23 +0200

    remove cleanup.js

--- cleanup.js
+++
@@ -1,9 +0,0 @@
-db.Pastedance.remove({$where: function() {
-        var now = parseInt((new Date).getTime() / 1000);
-        if (this.expires == "-1") {
-            return false;
-        } else {
-            return this.time + parseInt(this.expires)  <  now;
-        }
-    }
-});
