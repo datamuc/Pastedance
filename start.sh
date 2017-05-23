c5f13f3502b33e54b6b9644dfab160f0dd1279da
Danijel Tasov <m@rbfh.de>
2017-05-23 23:45:01 +0200

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/opt/python/2_007_000/lib
-    /opt/perl-5010001/bin/starman -D --preload-app --listen 127.0.0.1:6300 --workers 10 \
+    /opt/perl/perls/perl-5.16.3/bin/starman \
+        -D --preload-app --listen 127.0.0.1:6300 --workers 5 \
         -E production --pid $PWD/paste.pid bin/app.pl 2> logs/stderr.log
